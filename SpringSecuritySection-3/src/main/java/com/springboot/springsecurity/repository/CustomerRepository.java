9ca0dddd2a1379e45d06717cf35840b5dd9b5ba3
Nayanashree <nayanashree@kitservices.in>
2024-03-10 10:32:21 +0530

    Define user, Security using InMemoryUserDetailsManager, JdbcUserDetailsService & UserDetailsService

---
+++ SpringSecuritySection-3/src/main/java/com/springboot/springsecurity/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.springboot.springsecurity.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springboot.springsecurity.entity.Customer;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long>{
+	
+	 List<Customer> findByEmail(String email);
+
+}
