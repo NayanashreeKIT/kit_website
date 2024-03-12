e9de22929e80c33b2c2600f97f6086e754ab42fa
Nayanashree <nayanashree@kitservices.in>
2024-03-12 15:30:00 +0530

    Password management using PasswordEncoders

---
+++ SpringSecuritySection-4/src/main/java/com/springboot/springsecurity/SpringSecuritySection4Application.java
@@ -0,0 +1,13 @@
+package com.springboot.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySection4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySection4Application.class, args);
+	}
+
+}
---
+++ SpringSecuritySection-4/src/main/java/com/springboot/springsecurity/entity/Customer.java
@@ -0,0 +1,60 @@
+package com.springboot.springsecurity.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String pwd;
+	
+	@Column(nullable = false)
+	private String role;
+	
+	public  Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	
+
+}
