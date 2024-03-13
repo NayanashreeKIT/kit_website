630ca32219d68bad29892e14091a1da4c2f5574d
Nayanashree <nayanashree@kitservices.in>
2024-03-13 13:37:23 +0530

    Implement and Customize the Authentication Provider

---
+++ SpringSecuritySection-5/src/main/java/com/springboot/springsecurity/entity/Users.java
@@ -0,0 +1,27 @@
+package com.springboot.springsecurity.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "users")
+public class Users {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false)
+	private String username;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private int enabled;
+
+}
