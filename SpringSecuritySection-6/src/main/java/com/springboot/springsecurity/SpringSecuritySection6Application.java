9797a13497b5269d61e357aab81c0c70c4328a68
Nayanashree <nayanashree@kitservices.in>
2024-03-15 16:56:19 +0530

    Understand & handle CORs & CSRF issues inside Web app

---
+++ SpringSecuritySection-6/src/main/java/com/springboot/springsecurity/SpringSecuritySection6Application.java
@@ -0,0 +1,13 @@
+package com.springboot.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySection6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySection6Application.class, args);
+	}
+
+}
---
+++ SpringSecuritySection-6/src/test/java/com/springboot/springsecurity/SpringSecuritySection6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySection6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
